4290ba212c4da648e767fa8f7d03fff5e96f46ca
barbarasagredo <barpaz.sc@gmail.com>
2021-07-19 12:38:43 -0400

    Components created, initial route and CSS

---
+++ src/components/LeaderProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeaderProfile = () => {
+    return (
+        <div>
+            <h1>hola</h1>
+        </div>
+    )
+}
+
+export default LeaderProfile
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../components/components.css'
+
+const Login = () => {
+    return (
+        <main className= 'login'>
+            <h1>hola</h1>
+             </main>
+    )
+}
+
+export default Login
