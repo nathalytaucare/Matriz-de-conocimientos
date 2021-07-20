20c32a3b1be09a23df58af7b7bceafa4af9aa623
Nathaly Taucare <nathaly.tau@gmail.com>
2021-07-20 16:34:36 -0400

    changes DashboardLeader

--- src/components/DashboardLeader.jsx
+++ src/components/DashboardLeader.jsx
@@ -5,43 +5,7 @@ const DashboardLeader = () => {
   return (
     <Fragment>
      <Navbar />
-        <main className="dashboardLeader">
-        id="carouselExampleControls"
-        className="carousel slide"
-        data-bs-ride="carousel"
-        >
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="./imgCarousel/01.jpg" className="d-block w-100" alt="total" />
-                </div>
-                <div className="carousel-item">
-                    <img src="./imgCarousel/02.jpg" className="d-block w-100" alt="junior" />
-                </div>
-                <div className="carousel-item">
-                <img src="./imgCarousel/03.jpg" className="d-block w-100" alt="basic" />
-                </div>
-            </div>
-
-            <button 
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="prev"
-            >
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-
-            <button 
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="next"
-            >
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </main>    
+     
     </Fragment>
 
   );
