b318f0af1c9ed7a634dcb27affef6bd94fa01f41
danifran16 <daniela.frangb@gmail.com>
2021-07-20 16:14:36 -0400

    carousel dasboarCarousel 1 functional dashboard

--- src/components/DashboardLeader.jsx
+++ src/components/DashboardLeader.jsx
@@ -1,49 +1,15 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import Navbar from "./Navbar";
+import Carousel1 from "./Carousel1";
 
 const DashboardLeader = () => {
   return (
     <Fragment>
-     <Navbar />
-        <main className="dashboardLeader carousel slide"
-        id="carouselExampleControls"
-        data-bs-ride="carousel"
-        >
-
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="./imgCarousel/01.jpg" className="d-block w-100" alt="total" />
-                </div>
-                <div className="carousel-item">
-                    <img src="./imgCarousel/02.jpg" className="d-block w-100" alt="junior" />
-                </div>
-                <div className="carousel-item">
-                <img src="./imgCarousel/03.jpg" className="d-block w-100" alt="basic" />
-                </div>
-            </div>
-
-            <button 
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="prev"
-            >
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-
-            <button 
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleControls"
-                data-bs-slide="next"
-            >
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </main>    
+      <Navbar />
+        <div className="container">
+          <Carousel1 />
+        </div>     
     </Fragment>
-
   );
 };
 
