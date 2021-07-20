f8f90ef3aaf740a7f2f8b25f3d6d6bd4ba912ab5
luipalencia <luipalencia@gmail.com>
2021-07-20 12:09:28 -0400

    Create employee table view with respective CSS

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyCRgWBV33wVqHtSQJH6Ot2iHMIz_QtTLEA",
   authDomain: "matriz-conocimiento.firebaseapp.com",
@@ -7,4 +10,9 @@ const firebaseConfig = {
   appId: "1:762416556904:web:1fee4d068f0d90acea84dd"
 };
 
-module.exports = firebaseConfig;
+/* module.exports = firebaseConfig; */
+
+const fireb = firebase.initializeApp(firebaseConfig);
+const stores = fireb.firestore();
+
+export default stores; 
