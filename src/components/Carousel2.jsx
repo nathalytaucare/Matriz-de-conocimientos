8f87dd9b4c0c8d03e2dda4ed5597d0b1946188d4
danifran16 <daniela.frangb@gmail.com>
2021-07-20 22:19:51 -0400

    functional leader dashboard

--- src/components/Carousel2.jsx
+++ src/components/Carousel2.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './carousel.css';
 
-const Carousel1 = () => {
+const Carousel2 = () => {
   return ( 
     <section className="container img-fluid section-carousel ">
-      <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div id="carouselExampleDarkTech" className="carousel carousel-dark slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -16,36 +16,36 @@ const Carousel1 = () => {
         <div className="carousel-inner ">
             <div className="carousel-item active" data-bs-interval="10000">
               <div className="d-flex justify-content-center">
-                <img src="./imgCarousel/totalTech/total.jpg" className="d-block w-55" alt="total"/>
+                <img src="./imgCarousel/rankingTech/confluence.jpg" className="d-block w-100" alt="total"/>
               </div>
             </div> 
             <div className="carousel-item" data-bs-interval="2000">
               <div className="d-flex justify-content-center">
-                <img src="./imgCarousel/totalTech/junior.jpg" className="d-block w-55" alt="junior"/>              
+                <img src="./imgCarousel/rankingTech/github.jpg"  className="d-block w-100" alt="junior"/>              
               </div>
             </div>
             <div className="carousel-item">
               <div className="d-flex justify-content-center">
-                <img src="./imgCarousel/totalTech/basic.jpg" className="d-block w-55" alt="basic"/>              
+                <img src="./imgCarousel/rankingTech/jenkins.jpg"  className="d-block w-100" alt="basic"/>              
               </div>
             </div>
             <div className="carousel-item">
               <div className="d-flex justify-content-center">
-                <img src="./imgCarousel/totalTech/medium.jpg" className="d-block w-55" alt="medium"/>              
+                <img src="./imgCarousel/rankingTech/jira.jpg"  className="d-block w-100" alt="medium"/>              
               </div>
             </div>
             <div className="carousel-item">
               <div className="d-flex justify-content-center">
-                <img src="./imgCarousel/totalTech/expert.jpg" className="d-block w-55" alt="expert"/>              
+                <img src="./imgCarousel/rankingTech/ux-ui.jpg"  className="d-block w-100" alt="expert"/>              
               </div>
             </div>
 
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDarkTech" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDarkTech" data-bs-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
@@ -56,4 +56,4 @@ const Carousel1 = () => {
   );
 };
 
-export default Carousel1;
+export default Carousel2;
